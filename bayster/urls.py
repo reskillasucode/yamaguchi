e45c8ae5581d958f74a8e66627d8b502fc8e92bb
nahokoyamaguchi <nahoko80@hotmail.com>
2023-06-11 13:57:40 +0900

    code 15_land_list.html

--- bayster/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('land/', views.LandListView.as_view(), name='land-list'),
-    path('detail/<int:pk>/', views.LandDetailView.as_view(), name='land-detail'),
-    path('create/', views.LandCreateView.as_view(), name='land-create'),
-]
